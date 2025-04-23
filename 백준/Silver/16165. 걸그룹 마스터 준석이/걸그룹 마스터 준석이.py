d49818a60f09a2e2b290e4ec9cfc7a5d710c0fa8
wag192625 <106229215+wag192625@users.noreply.github.com>
2025-04-23 15:53:47 +0900

    [Silver III] Title: 걸그룹 마스터 준석이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.py
+++ 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.py
@@ -2,32 +2,40 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-# # N = 걸그룹의 수
-# # M = 문제의 수
-girl_group = {}
-
+# N = 걸그룹의 수
+# M = 문제의 수
+girl_group = []
 for _ in range(N):
-    # 팀 이름
+    # 팀의 이름
     team_name = input().strip()
-    # 걸그룹의 멤버 수
+    
+	# 걸그룹의 인원 수
     count_member = int(input())
-    # 멤버들의 이름
+	
+ 	# 멤버의 이름들
     members_name = []
     for _ in range(count_member):
         members_name.append(input().strip())
+    members_name.sort()
+#    print(members_name)
     
-    girl_group[team_name] = sorted(members_name)
-    
-for _ in range(M) :
-    quiz = input().strip()
-    quiz_type = input().strip()
-	# 팀 이름 (트와이스)
-    if quiz_type == '0' :
-		#멤버 한 줄당 한 명 출력
-        print('\n'.join(girl_group[quiz]))
-	# 멤버 이름
-    elif quiz_type == '1' :
-        for team_name, members in girl_group.items():
-            if quiz in members:
-                print(team_name)
-                break
+    girl_group.append([team_name, count_member, members_name])
+
+for _ in range(M):
+	quiz = input().strip()
+	quiz_type = input().strip()
+	if quiz_type == '0' :
+		# 팀 이름 (트와이스)
+		for i in girl_group :
+				#멤버 한 줄당 한 명 출력 
+			if quiz == i[0] :
+				for member in i[2]:
+					print(member)
+				break
+	elif quiz_type == '1' :
+		# 멤버 이름
+		for i in girl_group :
+			# 팀 이름 출력
+			if quiz in i[2] :
+				print(i[0])
+				break
